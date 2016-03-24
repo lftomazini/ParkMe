6f5c6a196710150bb22911b0bbd32a23e5d41a3b
AC Li <yl015@bucknell.edu>
2016-03-23 21:39:52 -0400

    Very basic app for trying out
    There is only a plain view of map. The icon is not original. I found it
    on google.

---
+++ ParkMe/ParkMe/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by AC on 2/29/16.
+//  Copyright © 2016 AC. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.mapView.showsUserLocation = true;
+        
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.delegate = self;
+        
+        // user activated automatic authorization info mode
+        let status = CLLocationManager.authorizationStatus()
+        if status == .NotDetermined || status == .Denied || status == .AuthorizedWhenInUse {
+            // present an alert indicating location authorization required
+            // and offer to take the user to Settings for the app via
+            // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+    
+    }
+    
+}
+
+extension ViewController: MKMapViewDelegate {
+}
