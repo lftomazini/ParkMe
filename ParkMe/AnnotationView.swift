50b5071935a94cd5c44e915d2aef0f379d127644
AC Li <yl015@bucknell.edu>
2016-04-10 18:54:21 -0400

    Map view improved
    Lots are circled and pinned on map

---
+++ ParkMe/AnnotationView.swift
@@ -0,0 +1,12 @@
+//
+//  AnnotationView.swift
+//  ParkMe
+//
+//  Created by AC on 4/10/16.
+//  Copyright © 2016 AC. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
--- ParkMe/ParkMe/ViewController.swift
+++ ParkMe/ParkMe/ViewController.swift
@@ -15,15 +15,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     var locationManager: CLLocationManager!
+    let BU_Latitude = 40.954582
+    let BU_Longitude = -76.883322
+    var BRKILot = Lots(filename: "BRKI")
+    var ACWSLot = Lots(filename: "ACWS")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.mapView.showsUserLocation = true;
+        self.mapView.showsUserLocation = false;
+        self.mapView.delegate = self;
         
-        locationManager = CLLocationManager()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-        locationManager.delegate = self;
+        
+        // Add pins for parking lots
+        mapView.addAnnotation(Annotation(coordinate: CLLocationCoordinate2D(latitude: BRKILot.midCoordinate.latitude, longitude: BRKILot.midCoordinate.longitude), title: "BreakIron", subtitle: "Density: Mild"));
+        mapView.addAnnotation(Annotation(coordinate: CLLocationCoordinate2D(latitude: ACWSLot.midCoordinate.latitude, longitude: ACWSLot.midCoordinate.longitude), title: "Academic West", subtitle: "Density: High"));
+        mapView.addAnnotation(Annotation(coordinate: CLLocationCoordinate2D(latitude: 40.957863, longitude: -76.885560), title: "Smith", subtitle: "Density: High"));
+        
         
         // user activated automatic authorization info mode
         let status = CLLocationManager.authorizationStatus()
@@ -34,18 +42,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.requestAlwaysAuthorization()
             locationManager.requestWhenInUseAuthorization()
         }
-        locationManager.startUpdatingLocation()
-        locationManager.startUpdatingHeading()
+        let initialLocation = CLLocation(latitude: BU_Latitude, longitude: BU_Longitude);
+        centerMapOnLocation(initialLocation);
+
+        addBoundary(BRKILot);
+        addBoundary(ACWSLot);
+        
     }
     
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last! as CLLocation
-        
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        
-        self.mapView.setRegion(region, animated: true)
+    let regionRadius: CLLocationDistance = 800
     
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 1.5, regionRadius * 1.5)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func addBoundary(lot: Lots) {
+        let polygon = MKPolygon(coordinates: &lot.boundary, count: lot.boundaryPointsCount)
+        mapView.addOverlay(polygon)
     }
     
     override func prefersStatusBarHidden() -> Bool {
@@ -55,4 +70,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 }
 
 extension ViewController: MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+            let polygonView = MKPolygonRenderer(overlay: overlay)
+            polygonView.strokeColor = UIColor.blueColor()
+        
+            return polygonView
+    }
+    
 }
