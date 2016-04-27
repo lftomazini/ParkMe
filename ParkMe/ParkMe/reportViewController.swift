fd0229c78fb62ad53e77ea47c2d6b10a394ce668
AC Li <yl015@bucknell.edu>
2016-04-26 22:26:47 -0400

    Added a stretchy header table to the report screen

--- ParkMe/ParkMe/reportViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  reportView.swift
-//  ParkMe
-//
-//  Created by AC on 4/26/16.
-//  Copyright © 2016 AC. All rights reserved.
-//
-
-import Foundation
---
+++ ParkMe/reportHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  reportHeaderView.swift
+//  ParkMe
+//
+//  Created by AC on 4/26/16.
+//  Copyright © 2016 AC. All rights reserved.
+//
+
+import UIKit
+
+class reportHeaderView: UIView {    
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+}
