088b66424115849e588d3a7cc50de08ed9b43f0d
AC Li <yl015@bucknell.edu>
2016-04-28 01:52:54 -0400

    Report screen finished
    Added two buttons to report a parking lot is full and to report issues.
    Included a third-party framework to show customized alerts.

--- ParkMe/Pods/Target Support Files/Pods-ParkMeTests/Pods-ParkMeTests-frameworks.sh
+++ ParkMe/Pods/Target Support Files/Pods-ParkMeTests/Pods-ParkMeTests-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-ParkMeTests/Firebase.framework"
+  install_framework "Pods-ParkMeTests/SCLAlertView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-ParkMeTests/Firebase.framework"
+  install_framework "Pods-ParkMeTests/SCLAlertView.framework"
 fi
--- ParkMe/reportViewController.swift
+++ ParkMe/reportViewController.swift
@@ -90,12 +90,78 @@ extension reportViewController: UITableViewDataSource
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 4
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
         let cell = tableView.dequeueReusableCellWithIdentifier("Option Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        if (indexPath.row == 2 ) {
+            let reportFullBtn : UIButton = UIButton(type: UIButtonType.Custom) as UIButton
+            reportFullBtn.frame = CGRectMake(40, 60, 250, 30)
+            let cellHeight: CGFloat = 44.0
+            reportFullBtn.center = CGPoint(x: view.bounds.width / 2.0, y: cellHeight / 2.0)
+            reportFullBtn.setTitleColor(UIColorFromRGB(0x0D94FC), forState: UIControlState.Normal)
+            reportFullBtn.addTarget(self, action: #selector(reportViewController.reportFull(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            reportFullBtn.setTitle("Report this lot is full", forState: UIControlState.Normal)
+            reportFullBtn.titleLabel?.font = UIFont(name: "ArialMT", size: 16)!
+            
+            cell.addSubview(reportFullBtn)
+        }
+        
+        if (indexPath.row == 3) {
+            let reportIssueBtn : UIButton = UIButton(type: UIButtonType.Custom) as UIButton
+            reportIssueBtn.frame = CGRectMake(40, 60, 250, 30)
+            let cellHeight: CGFloat = 44.0
+            reportIssueBtn.center = CGPoint(x: view.bounds.width / 2.0, y: cellHeight / 2.0)
+            reportIssueBtn.setTitleColor(UIColorFromRGB(0x0D94FC), forState: UIControlState.Normal)
+            reportIssueBtn.addTarget(self, action: #selector(reportViewController.reportIssue(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            reportIssueBtn.setTitle("Report an issue", forState: UIControlState.Normal)
+            reportIssueBtn.titleLabel?.font = UIFont(name: "ArialMT", size: 16)!
+            
+            cell.addSubview(reportIssueBtn)
+        }
         return cell
+    }
+    
+    @IBAction func reportFull(sender: AnyObject) {
+        
+        let alertView = SCLAlertView()
+        alertView.addButton("Yes") {
+            SCLAlertView().showSuccess("Thank you", subTitle: "Your report has been recorded!")
+        }
+        alertView.addButton("No") {
+            
+        }
+        alertView.showCloseButton = false
+        alertView.showWarning("Waring", subTitle: "Are you sure you want to report this lot is full?")
+        
+    }
+    
+    @IBAction func reportIssue(sender: AnyObject) {
+        
+        let alertView = SCLAlertView()
+        let issue = alertView.addTextField("Please enter your issue")
+        
+        alertView.addButton("Submit") {
+            if ( issue.text != "" ) {
+                SCLAlertView().showSuccess("Thank you", subTitle: "Your issue has been sent to us and we will work on it.")
+            } else {
+                SCLAlertView().showError("Error", subTitle: "No issue entered!")
+            }
+        }
+        alertView.showEdit("Report an issue", subTitle: "Please tell us what your issue is")
+        
+    }
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
 
